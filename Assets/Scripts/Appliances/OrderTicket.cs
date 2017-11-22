a28d3933156784de3533dfa0ad8d05799a86f2af
Thunder Clonch <thunderjoe1000@gmail.com>
2017-11-21 19:03:56 -0500

    More work on orders.
    Still needs work.

---
+++ Assets/Scripts/Appliances/OrderTicket.cs
@@ -0,0 +1,19 @@
+﻿/*******************************************************************************************************
+Class Name:     OrderTicket
+Author’s Name:  Thunder Clonch
+Created Date:   11/21/2017
+Description:    The class stores and displays the details of an order to the player.
+
+Last Edited:  
+Last Editor: 
+Last Edit Description:
+
+*******************************************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderTicket : MonoBehaviour
+{
+
+}
