2d2a78f539b346f61cbef45d109e463e74984341
Thunder Clonch <thunderjoe1000@gmail.com>
2017-11-28 18:25:39 -0500

    Some Meta changes.
    On unity's backend. Nothing in the project changed.

--- Assets/Scripts/Appliances/FoodTray.cs
+++ Assets/Scripts/Appliances/FoodTray.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+﻿/*******************************************************************************************************
+Class Name:     FoodTray
+Author’s Name:  Thunder Clonch
+Created Date:   11/21/2017
+Description:    This class handles a tray that can hold an order ticket and several foods. This is referenced
+by the order table for testing.
+
+Last Edited:  
+Last Editor: 
+Last Edit Description:
+
+*******************************************************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodTray : MonoBehaviour {
+public class FoodTray : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
