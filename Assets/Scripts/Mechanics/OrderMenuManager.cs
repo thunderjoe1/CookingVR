cefe70a5c7a8dea38fe431c0322de0261b15f729
Xavier Fox <xf899715@ohio.edu>
2018-02-18 19:44:23 -0500

    More Work
    Made a prototype of the sceen and began making order management classes.

---
+++ Assets/Scripts/Mechanics/OrderMenuManager.cs
@@ -0,0 +1,23 @@
+﻿/*******************************************************************************************************
+Class Name:     OrderMenuManager
+Author’s Name:  Thunder Clonch
+Created Date:   02/18/2018
+Description:    The class which manages the screens upon which the orders are displayed to the player.
+
+Last Edited:  
+Last Editor: 
+Last Edit Description:
+
+*******************************************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderMenuManager : MonoBehaviour
+{
+    public List<GameObject> orderScreen = new List<GameObject>();
+
+
+
+
+}
