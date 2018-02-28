a32f271024d26729bfbdb23ab2cda9bf0286a9be
William Gray <willgraygames@gmail.com>
2018-02-27 20:49:13 -0500

    Room Customization Progress 1

--- Assets/Scripts/UI/PlaceableObject.cs
+++ Assets/Scripts/UI/PlaceableObject.cs
@@ -5,14 +5,62 @@ using UnityEngine;
 public class PlaceableObject : MonoBehaviour {
 
 	public GameObject objectReference;
+	public applianceSize mySize;
+	public string myObjectName;
+
+	Transform currentSpace;
+	MeshRenderer myRender;
+	NewtonVR.NVRInteractableItem NVRReference;
+	bool isPlaced;
 
 	// Use this for initialization
 	void Start () {
-		
+		myRender = GetComponent<MeshRenderer> ();
+		NVRReference = GetComponent<NewtonVR.NVRInteractableItem> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		SnapObject ();
+		DisplayGhosts ();
+	}
+
+	void SnapObject () {
+		if (currentSpace != null && isPlaced == false) {
+			if (GetComponent<NewtonVR.NVRInteractableItem> ().IsAttached == false) {
+				transform.position = currentSpace.transform.position;
+				transform.rotation = currentSpace.transform.rotation;
+				myRender.enabled = false;
+				objectReference.SetActive (true);
+				GetComponent<Rigidbody> ().isKinematic = true;
+				currentSpace.gameObject.SetActive (false);
+				isPlaced = true;
+				NVRReference.CanAttach = false;
+			}
+		}
+	}
+
+	void DisplayGhosts() {
+		if (NVRReference.IsAttached == true) {
+			foreach (GameObject ghost in RoomController.Instance.ghostList) {
+				if (ghost.name == mySize + " Object Space") {
+					ghost.GetComponentInChildren<MeshFilter> ().mesh = objectReference.GetComponent<MeshFilter> ().mesh;
+
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider coll) {
+		if (coll.tag == "Object Space") {
+			if (coll.GetComponent<ObjectSpace> ().spaceSize == mySize) {
+				currentSpace = coll.transform;
+
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider coll) {
+		currentSpace = null;
 	}
 }
---
+++ Assets/Scripts/Utility/RoomController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour {
+
+	public static RoomController Instance { get; protected set; }
+
+	public List<GameObject> ghostList = new List<GameObject> ();
+
+
+	void Awake () {
+		if (Instance != null) {
+
+		} else {
+			Instance = this;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
