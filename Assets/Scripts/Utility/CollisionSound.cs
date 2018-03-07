a41186ed453024da4c350aaa5cf0553962a41019
thunderjoe1 <thunderjoe1000@gmail.com>
2018-03-06 19:33:08 -0500

    Little more scene work and minor bugfix.
    Audio deletes properly and tray is set up properly.

--- Assets/Scripts/Utility/CollisionSound.cs
+++ Assets/Scripts/Utility/CollisionSound.cs
@@ -13,6 +13,6 @@ public class CollisionSound : MonoBehaviour
         temp = Instantiate(soundMaker, col.transform.position, Quaternion.identity);
         temp.GetComponent<AudioSource>().clip = collisionSound;
         temp.GetComponent<AudioSource>().Play();
-        Destroy(gameObject, 1f);
+		Destroy(temp, 1f);
     }
 }
