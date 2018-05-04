52e1f228e58acce8c93dfd1013f2476bba2531cd
William Gray <willgraygames@gmail.com>
2018-05-03 20:23:57 -0400

    Conveyor Works

---
+++ Assets/Scripts/Mechanics/ConveyorBelt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+
+	public float force;
+
+	void OnTriggerStay (Collider col) {
+		if (col.GetComponent<Rigidbody> ()) {
+			col.GetComponent<Rigidbody> ().velocity = new Vector3(force, 0, 0);
+		}
+	}
+}
